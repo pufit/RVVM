45c61bba5b2bde6605167a472ed861e6bf4d2f07
Sol Astrius <me@danielsol.dev>
2026-04-29 03:40:59 +0300

    parport-pci: fix copyright email

--- src/devices/parport-pci.h
+++ src/devices/parport-pci.h
@@ -1,6 +1,6 @@
 /*
 parport-pci.h - PCI Parallel Port (NetMos/MosChip MCS9900)
-Copyright (C) 2026  Sol Astrius <claude@danielsol.dev>
+Copyright (C) 2026  Sol Astrius <sol@astrius.ink>
 
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
